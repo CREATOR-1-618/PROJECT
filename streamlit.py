b8e5047c9f2ef1afb61ec2af4a91348217635199
CREATOR-1-618 <62265798+CREATOR-1-618@users.noreply.github.com>
2022-10-23 14:49:54 +0200

    s

--- streamlit.py
+++ streamlit.py
@@ -21,15 +21,20 @@ if __name__ == "__main__":
 
     with st.sidebar:
         selected = option_menu("Trafic Analysis",
-                               ["Traffic analysis"],
-                               icons=['signpost'], menu_icon="cast", default_index=1)
+                               ["Streets with the highest congestion", "The average car density of the city", "Average time needed", "Mouhamadou", "Anicette", "Megane"],
+                               icons=['signpost', 'car-front', 'alarm', 'person', 'person', 'person'
+                                      ], menu_icon="cast", default_index=1)
 
     st.title("Welcome to Beijing Traffic Analysis ")
 
 
-    if selected == "Traffic Analysis":
+    if selected == "Streets with the highest congestion":
         file = pd.read_feather('dataf.csv')
+        st.title('Streets with the highest congestion')
+        highest_congestion(file)
 
+    if selected == "The average car density of the city":
+        file = pd.read_feather('dataf.csv')
         st.title("The average car density of the city")
         car_density(file)
 
