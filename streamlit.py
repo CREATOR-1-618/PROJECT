5946a53b2eb9cb1f2dad56cb1954bc5b2b13ab58
CREATOR-1-618 <62265798+CREATOR-1-618@users.noreply.github.com>
2022-10-25 16:42:42 +0200

    changenow

--- streamlit.py
+++ streamlit.py
@@ -17,24 +17,52 @@ def car_density(data):
     fig = px.bar(densi10)
     st.plotly_chart(fig)
 
+def hour_study(data) :
+    df=data[['adresse','hour']]
+    hour_study = df.groupby(['hour']).agg({"adresse":"count"})
+    fig = px.bar(hour_study)
+    st.plotly_chart(fig)
+
+def street_hour_study(data,street):
+    data=data[['adresse','hour']]
+    street = data.where(data['adresse']==street)
+    street.dropna()
+    hour_study = street.groupby(['hour']).agg({"adresse":"count"})
+    fig = px.bar(hour_study)
+    st.plotly_chart(fig)
+
+def street_list_hour(data):
+    adresse = data['adresse'].drop_duplicates().values.tolist()
+    sf = st.selectbox(
+        "Selection the street you want",  # Drop Down Menu Name
+        adresse
+    )
+    street_hour_study(data, sf)
+
 if __name__ == "__main__":
 
     with st.sidebar:
         selected = option_menu("Trafic Analysis",
-                               ["Streets with the highest congestion", "The average car density of the city", "Average time needed", "Mouhamadou", "Anicette", "Megane"],
-                               icons=['signpost', 'car-front', 'alarm', 'person', 'person', 'person'
-                                      ], menu_icon="cast")
+                               ["Traffic Analysis","Mouhamadou", "Anicette", "Megane"],
+                               icons=['signpost', 'person', 'person', 'person'
+                                      ], menu_icon="cast", default_index=1)
 
     st.title("Welcome to Beijing Traffic Analysis ")
 
 
-    if selected == "Streets with the highest congestion":
+    if selected == "Traffic Analysis":
+
         file = pd.read_feather('dataf.csv')
         st.title('Streets with the highest congestion')
         highest_congestion(file)
-
+        file = pd.read_feather('dataf.csv')
         st.title("The average car density of the city")
         car_density(file)
+        st.title('Hour congestion')
+        hour_study(file)
+        street_list_hour(file)
+
+
 
 
 
